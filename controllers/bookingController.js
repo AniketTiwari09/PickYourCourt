bcca47cee0e76bcc05e22dccfefbac5ad86211db
AniketTiwari09 <atiwari7@uncc.edu>
2024-04-11 22:28:40 -0400

    Commit

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,7 +3,7 @@ const Court = require('../models/court');
 const Booking = require('../models/booking');
 const Offer = require('../models/offer');
 const User = require('../models/user');
-const timeSlotArr = ['9:00AM - 10:00AM', '10:00 AM - 11:00 AM', '11:00 AM - 12:00 PM', '12:00 PM - 1:00 PM', '01:00 AM - 02:00 PM', '02:00 AM - 03:00 PM', '03:00 AM - 04:00 PM', '04:00 AM - 05:00 PM', '05:00 AM - 06:00 PM', '06:00 AM - 07:00 PM', '07:00 AM - 08:00 PM' , '08:00 AM - 09:00 PM'];
+const timeSlotArr = ['9:00AM - 10:00AM', '10:00 AM - 11:00 AM', '11:00 AM - 12:00 PM', '12:00 PM - 1:00 PM', '01:00 PM - 02:00 PM', '02:00 PM - 03:00 PM', '03:00 PM - 04:00 PM', '04:00 PM - 05:00 PM', '05:00 PM - 06:00 PM', '06:00 PM - 07:00 PM', '07:00 PM - 08:00 PM' , '08:00 PM - 09:00 PM'];
 const sendEmail = require('../middlewares/emailService');
 const booking = require('../models/booking');
 
